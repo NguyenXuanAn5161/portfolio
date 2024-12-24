7b20dab2f7d7aac6ff75b0a585ea957d80bf27e5
Nguyen Xuan An <nguyenxuanan20021010@gmail.com>
2024-12-24 12:48:53 +0700

    refactor: icon skill

--- src/components/Card/CardSkill.jsx
+++ src/components/Card/CardSkill.jsx
@@ -1,33 +1,38 @@
 import { Card } from "antd";
 import { useState } from "react";
+import "../../styles/theme.css";
+import { JS } from "../Svg/MySvg";
 
 const CardSkill = ({ data }) => {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <Card
-      title={data?.technology}
-      bordered={false}
-      style={{
-        width: 250,
-        boxShadow: hovered
-          ? "0px 8px 16px rgba(0, 0, 0, 0.2)"
-          : "0px 4px 6px rgba(0, 0, 0, 0.1)",
-        borderRadius: "8px",
-        transition: "box-shadow 0.3s ease-in-out",
-      }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <p>{data?.description}</p>
-      {Array.isArray(data?.skills) && (
-        <ul>
-          {data?.skills.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      )}
-    </Card>
+    <div className="columnCustom gap-10">
+      <JS />
+      <Card
+        title={data?.technology}
+        bordered={false}
+        style={{
+          width: 250,
+          boxShadow: hovered
+            ? "var(--boxShadow-hovered)"
+            : "var(--boxShadow-unhovered)",
+          borderRadius: "8px",
+          transition: "box-shadow 0.3s ease-in-out",
+        }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <p>{data?.description}</p>
+        {Array.isArray(data?.skills) && (
+          <ul>
+            {data?.skills.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        )}
+      </Card>
+    </div>
   );
 };
 
